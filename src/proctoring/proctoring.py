484401bc29c4ac82090b294877b783de40733055
Vincent Nord Lundström <vnl@kth.se>
2025-04-09 08:44:48 +0200

    Add args for demo mode

--- src/proctoring/proctoring.py
+++ src/proctoring/proctoring.py
@@ -6,16 +6,16 @@ from proctoring.gaze import Gaze
 from multiprocessing import Process, Queue
 
 class Proctoring(object):
-    def __init__(self):
+    def __init__(self, demo):
         self.gaze_queue = Queue()
 
-        gaze = Process(target=self.run_gaze, args=(self.gaze_queue,))
+        gaze = Process(target=self.run_gaze, args=(self.gaze_queue,demo,))
         gaze.start()
         gaze_recieve = Process(target=self.listen_for_gaze)
         gaze_recieve.start()
     
-    def run_gaze(self, queue):
-        gaze_instance = Gaze(queue)
+    def run_gaze(self, queue, demo):
+        gaze_instance = Gaze(queue, demo)
         gaze_instance.run()
 
     def listen_for_gaze(self):
