549f3b3646db8789c66cb84f11916174efd9ae06
Vincent Nord Lundström <vnl@kth.se>
2025-04-24 18:48:44 +0200

    Merge main

--- src/proctoring/proctoring.py
+++ src/proctoring/proctoring.py
@@ -26,24 +26,50 @@ class Proctoring:
             demo (bool): Run in demo mode if True.
         """
         self._demo = demo 
+
         self._gaze_queue = Queue()
-        self._gaze_process = Process(target=self._run_gaze, args=(self._gaze_queue,))
-        self._gaze_instance = None
-        self._gaze_recieve = Process(target=self._listen_for_gaze)
+        self._process_queue = Queue()
+
+        self._processes = {
+            "gaze": None,
+            "gaze_recieve": None,
+            "process_monitor": None,
+            "process_monitor_recieve": None,
+            "browser": None
+        }
+
         self.running = False
 
     def start_exam(self):
-        self._gaze_process.start()
-        self._gaze_recieve.start()
+        if self.running == True: return
+        self._processes["gaze"] = Process(target=self._run_gaze, args=(self._gaze_queue,))
+        self._processes["gaze_recieve"] = Process(target=self._listen_for_gaze)
+        self._processes["process_monitor"] = Process(target=self._run_process_monitor, args=(self._process_queue,))
+        self._processes["process_monitor_recieve"] = Process(target=self._listen_for_processes)
+        self._processes["browser"] = Process(target=self._run_browser)
+
+        for _, process in self._processes.items():
+            process.start()
+
         self.running = True
 
     def end_exam(self):
-        self._gaze_process.terminate()
-        self._gaze_recieve.terminate()
+        if self.running == False: return
+        
+        for name, process in self._processes.items():
+            process.terminate()
+            self._processes[name] = None
+
         self.running = False
     
     def _run_gaze(self, queue):
-        self._gaze_instance = Gaze(queue, self._demo)
+        Gaze(queue, self._demo).run()
+
+    def _run_process_monitor(self, queue):
+        ProcessMonitor(queue).run()
+
+    def _run_browser(self):
+        Browser().run()
 
     def _listen_for_gaze(self):
         while True:
@@ -51,6 +77,14 @@ class Proctoring:
                 msg = self._gaze_queue.get()
                 title = "Gazeaway"
                 message=f"Identified user gazeaway for: {msg:.2f}s"
+                self._notify(title, message)
+
+    def _listen_for_processes(self):
+        while True:
+            if not self._process_queue.empty():
+                msg = self._process_queue.get()
+                title = "Process identified"
+                message=f"Identified new process: {msg}s"
                 self._notify(title, message)
 
     def _notify(self, title, message):
